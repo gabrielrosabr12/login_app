40515debc3b67ffb2373f38d4d90d12095a9c5bc
Gabriel Rosa <gabrielrosaobr@gmail.com>
2026-07-09 18:09:42 -0300

    fix: Fixing,adjust and take user role

--- src/app/services/roles-user-service.ts
+++ src/app/services/roles-user-service.ts
@@ -1,7 +1,32 @@
-import { Service } from '@angular/core';
+import { AuthUserService } from './auth-user-service';
+import { inject, Service } from '@angular/core';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
+
+interface TokenDecodificado{
+
+}
 
 @Service()
 export class RolesUserService {
+  authUserService:AuthUserService = inject(AuthUserService);
+
+  getRole(){
+    const token = this.authUserService.getToken();
+
+    if(token && token != null && token != 'null'){
+      try{
+        const tokenDecodificado:any = jwtDecode(token);
+        localStorage.setItem('roles',tokenDecodificado.role);
+      } catch(error){
+
+      }
+
+
+    }
+
+  }
+
+
 
 
 
